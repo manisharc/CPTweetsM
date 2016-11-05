809fd0caf0d6d07ba45548322b5a8da12f3a52f3
Roy Choudhury <chmanish@f45c898c8a4d.ant.amazon.com>
2016-11-05 10:40:20 -0700

    works as before but in fragments

---
+++ app/src/main/java/com/codepath/apps/CPTweetsM/fragments/HomeTimelineFragment.java
@@ -0,0 +1,147 @@
+package com.codepath.apps.CPTweetsM.fragments;
+
+import android.os.Bundle;
+import android.widget.Toast;
+
+import com.codepath.apps.CPTweetsM.TwitterApplication;
+import com.codepath.apps.CPTweetsM.database.TweetDatabase;
+import com.codepath.apps.CPTweetsM.models.Tweet;
+import com.codepath.apps.CPTweetsM.models.Tweet_Table;
+import com.codepath.apps.CPTweetsM.models.User;
+import com.codepath.apps.CPTweetsM.network.TwitterClient;
+import com.loopj.android.http.JsonHttpResponseHandler;
+import com.raizlabs.android.dbflow.config.DatabaseDefinition;
+import com.raizlabs.android.dbflow.config.FlowManager;
+import com.raizlabs.android.dbflow.sql.language.Delete;
+import com.raizlabs.android.dbflow.sql.language.SQLite;
+import com.raizlabs.android.dbflow.structure.database.DatabaseWrapper;
+import com.raizlabs.android.dbflow.structure.database.transaction.ITransaction;
+import com.raizlabs.android.dbflow.structure.database.transaction.ProcessModelTransaction;
+import com.raizlabs.android.dbflow.structure.database.transaction.Transaction;
+
+import org.json.JSONArray;
+import org.json.JSONObject;
+
+import java.util.List;
+
+import cz.msebera.android.httpclient.Header;
+
+/**
+ * Created by chmanish on 11/3/16.
+ */
+public class HomeTimelineFragment extends TweetsListFragment{
+
+    private TwitterClient client;
+    @Override
+    public void onCreate(Bundle savedInstanceState){
+        super.onCreate(savedInstanceState);
+        client = TwitterApplication.getRestClient(); //singleton client
+
+        if (isOnline)
+        // Retrieve from the network
+            populateTimeline(false, true);
+        else {
+            Toast.makeText(getContext(), "Cannot retrieve tweets, since you are offline!", Toast.LENGTH_LONG).show();
+            //Retrieve from the database
+            int curSize = adapter.getItemCount();
+            //get from database
+            List<Tweet> tweetsFromDb = getAllTweetsFromDatabase();
+            tweets.addAll(tweetsFromDb);
+            adapter.notifyItemRangeInserted(curSize, (tweetsFromDb.size())-1);
+        }
+
+
+
+    }
+
+    // Send an api request to get the timeline json
+    // Fill the view by creating the tweet objects from the json
+    @Override
+    protected void populateTimeline(final boolean isRefresh, final boolean isFirstCall) {
+        super.populateTimeline(isRefresh, isFirstCall);
+        client.getHomeTimeline(new JsonHttpResponseHandler(){
+            // Success
+
+            @Override
+            public void onSuccess(int statusCode, Header[] headers, JSONArray response) {
+                int curSize = adapter.getItemCount();
+                tweets.addAll(Tweet.fromJSONArray(response));
+                max_id = tweets.get(tweets.size()-1).getUid();
+
+                //Update database here
+                if (isFirstCall | isRefresh){
+                    deleteTables();
+
+                }
+                addToDataBase();
+                adapter.notifyItemRangeInserted(curSize, (Tweet.fromJSONArray(response)).size());
+                if (isRefresh)
+                    swipeContainer.setRefreshing(false);
+            }
+
+            @Override
+            public void onFailure(int statusCode, Header[] headers, Throwable throwable, JSONObject errorResponse) {
+
+            }
+        }, max_id);
+    }
+
+    private void deleteTables(){
+        DatabaseDefinition database = FlowManager.getDatabase(TweetDatabase.class);
+        Transaction transaction = database.beginTransactionAsync(new ITransaction() {
+            @Override
+            public void execute(DatabaseWrapper databaseWrapper) {
+                Delete.table(User.class);
+                Delete.table(Tweet.class);
+            }
+
+
+        }).build();
+        transaction.execute(); // execute
+        transaction.cancel();
+        // attempt to cancel before its run. If it's already ran, this call has no effect.
+
+
+    }
+
+    private List<Tweet> getAllTweetsFromDatabase(){
+        // Order the return based on createdDate
+        List<Tweet> tweetList = SQLite.select().
+                from(Tweet.class).orderBy(Tweet_Table.uid, false).queryList();
+        return tweetList;
+    }
+
+    private void addToDataBase(){
+        FlowManager.getDatabase(TweetDatabase.class)
+                .beginTransactionAsync(new ProcessModelTransaction.Builder<>(
+                        new ProcessModelTransaction.ProcessModel<Tweet>() {
+                            @Override
+                            public void processModel(Tweet tweet) {
+                                User user = tweet.getUser();
+                                user.save();
+                                tweet.save();
+                            }
+                        }).addAll(tweets).build())  // add elements (can also handle multiple)
+                .error(new Transaction.Error() {
+                    @Override
+                    public void onError(Transaction transaction, Throwable error) {
+
+                    }
+                })
+                .success(new Transaction.Success() {
+                    @Override
+                    public void onSuccess(Transaction transaction) {
+
+                    }
+                }).build().execute();
+
+    }
+
+    public void addTweet(Tweet newTweet) {
+        tweets.addFirst(newTweet);
+        //Update database here
+        newTweet.save();
+        adapter.notifyItemInserted(0);
+        rvTweets.scrollToPosition(0);   // index 0 position
+    }
+}
---
+++ app/src/main/java/com/codepath/apps/CPTweetsM/fragments/TweetsListFragment.java
@@ -0,0 +1,178 @@
+package com.codepath.apps.CPTweetsM.fragments;
+
+import android.content.Context;
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.design.widget.FloatingActionButton;
+import android.support.v4.app.Fragment;
+import android.support.v4.widget.SwipeRefreshLayout;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageButton;
+import android.widget.Toast;
+
+import com.codepath.apps.CPTweetsM.R;
+import com.codepath.apps.CPTweetsM.activities.TimelineActivity;
+import com.codepath.apps.CPTweetsM.adapters.TweetsAdapter;
+import com.codepath.apps.CPTweetsM.databinding.FragmentComposeTweetBinding;
+import com.codepath.apps.CPTweetsM.models.Tweet;
+import com.codepath.apps.CPTweetsM.network.NetworkStatus;
+import com.codepath.apps.CPTweetsM.network.TwitterClient;
+import com.codepath.apps.CPTweetsM.utility.DividerItemDecoration;
+import com.codepath.apps.CPTweetsM.utility.EndlessRecyclerViewScrollListener;
+import com.codepath.apps.CPTweetsM.utility.ItemClickSupport;
+
+import java.util.LinkedList;
+
+/**
+ * Created by chmanish on 11/3/16.
+ */
+public class TweetsListFragment extends Fragment  {
+
+    private FragmentComposeTweetBinding binding;
+    private TwitterClient client;
+    public LinkedList<Tweet> tweets;
+    public TweetsAdapter adapter;
+    public static long max_id = 0;
+    protected SwipeRefreshLayout swipeContainer;
+    private ImageButton ibReplyToTweet;
+    protected RecyclerView rvTweets;
+    protected FloatingActionButton fab;
+    protected LinearLayoutManager linearLayoutManager;
+    public boolean isOnline = true;
+
+
+    public interface TweetsListActionListener {
+        public void onAddNewTweet();
+        public void onReplyTweet(Tweet tweet);
+        public void onItemClickDetailView(Tweet tweet);
+
+    }
+
+    public TweetsListActionListener actionListener;
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup parent,
+                             Bundle savedInstanceState) {
+        //binding = DataBindingUtil.setContentView(getActivity(), R.layout.fragment_compose_tweet);
+
+        return inflater.inflate(R.layout.fragment_tweets_list, parent, false);
+
+    }
+
+    @Override
+    public void onAttach(Context context) {
+        super.onAttach(context);
+        if (context instanceof TimelineActivity) {
+            actionListener = (TimelineActivity)context;
+        }
+    }
+
+    @Override
+    public void onViewCreated(View view, @Nullable Bundle savedInstanceState) {
+        rvTweets = (RecyclerView) view.findViewById(R.id.rvTweets);
+        tweets = new LinkedList<Tweet>();
+        setupViews(view);
+        NetworkStatus networkStatus = NetworkStatus.getSharedInstance();
+        isOnline = networkStatus.checkNetworkStatus(getContext());
+
+    }
+
+    public void setupViews(View view) {
+        //rvTweets = binding.rvTweets;
+        rvTweets = (RecyclerView)  view.findViewById(R.id.rvTweets);
+        //swipeContainer = binding.swipeContainer;
+        swipeContainer = (SwipeRefreshLayout) view.findViewById(R.id.swipeContainer);
+        //FloatingActionButton fab = binding.fabCompose;
+        fab = (FloatingActionButton) view.findViewById(R.id.fabCompose);
+        adapter = new TweetsAdapter(getContext(), tweets);
+        rvTweets.setAdapter(adapter);
+        linearLayoutManager = new LinearLayoutManager(getActivity());
+        rvTweets.setLayoutManager(linearLayoutManager);
+        rvTweets.addItemDecoration(
+                new DividerItemDecoration(getActivity(), R.drawable.divider));
+
+        rvTweets.addOnScrollListener(new EndlessRecyclerViewScrollListener(linearLayoutManager) {
+            @Override
+            public void onLoadMore(int page, int totalItemsCount) {
+                if (isOnline)
+                    populateTimeline(false, false);
+
+            }
+        });
+
+        // Setup refresh listener which triggers new data loading
+        swipeContainer.setOnRefreshListener(new SwipeRefreshLayout.OnRefreshListener() {
+            @Override
+            public void onRefresh() {
+                // Refresh list of tweets
+                if (isOnline) {
+                    int size = tweets.size();
+                    tweets.clear();
+                    adapter.notifyItemRangeRemoved(0, size);
+                    //Clear database
+                    max_id = 0;
+                    populateTimeline(true, false);
+                }
+                else {
+                    Toast.makeText(getContext(), "You are offline. Can't refresh tweets", Toast.LENGTH_LONG).show();
+
+
+                }
+            }
+        });
+        // Configure the refreshing colors
+        swipeContainer.setColorSchemeResources(android.R.color.holo_blue_bright,
+                android.R.color.holo_green_light,
+                android.R.color.holo_orange_light,
+                android.R.color.holo_red_light);
+
+
+
+        adapter.setOnTweetClickListener(new TweetsAdapter.OnTweetClickListener() {
+            @Override
+            public void onItemClick(View view, int position) {
+                if ( actionListener != null ) {
+                    actionListener.onReplyTweet(tweets.get(position));
+                }
+
+            }
+        });
+
+
+
+        ItemClickSupport.addTo(rvTweets).setOnItemClickListener(
+                new ItemClickSupport.OnItemClickListener() {
+                    @Override
+                    public void onItemClicked(RecyclerView recyclerView, int position, View v) {
+                        if ( actionListener != null ) {
+                            actionListener.onItemClickDetailView(tweets.get(position));
+                        }
+
+                    }
+                }
+        );
+
+
+
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if ( actionListener != null ) {
+                    actionListener.onAddNewTweet();
+                }
+            }
+        });
+    }
+
+    protected void populateTimeline(final boolean isRefresh, final boolean isFirstCall){
+        // Overridden in HomeTimeline
+    }
+
+
+
+
+}
