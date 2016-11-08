7d12f488cdd0150156950db97e75fa4ebf39c5fc
Roy Choudhury <chmanish@f45c898c8a4d.ant.amazon.com>
2016-11-07 21:08:11 -0800

    updating network code

--- app/src/main/java/com/codepath/apps/CPTweetsM/network/NetworkStatus.java
+++ app/src/main/java/com/codepath/apps/CPTweetsM/network/NetworkStatus.java
@@ -23,15 +23,14 @@ public class NetworkStatus {
     }
 
     public boolean isOnline() {
-        /*Runtime runtime = Runtime.getRuntime();
+        Runtime runtime = Runtime.getRuntime();
         try {
             Process ipProcess = runtime.exec("/system/bin/ping -c 1 8.8.8.8");
             int     exitValue = ipProcess.waitFor();
             return (exitValue == 0);
         } catch (IOException e)          { e.printStackTrace(); }
         catch (InterruptedException e) { e.printStackTrace(); }
-        return false;*/
-        return true;
+        return false;
     }
 
     public boolean checkNetworkStatus(Context c){
